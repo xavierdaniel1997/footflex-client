63897dcf859ea82a792ff77a1fd31947cffe813f
xavierdaniel1997 <danielcx532@gmail.com>
2025-11-17 20:59:57 +0530

    update the api root

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: `https://api.footflexonline.shop/api`,
+    baseURL: `http://localhost:8000/api`,
     headers: {
         "Content-Type": "application/json"
     },
