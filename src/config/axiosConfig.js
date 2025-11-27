948c9e78435dd52cdfa60203b5e57fa0daa7732d
xavierdaniel1997 <danielcx532@gmail.com>
2025-11-27 20:58:10 +0530

    change the server origin url

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -2,6 +2,8 @@
 
 import axios from "axios";
 
+console.log("process.env.REACT_APP_SERVER_ORIGIN", process.env.REACT_APP_SERVER_ORIGIN)
+
 const API_URL = process.env.REACT_APP_SERVER_ORIGIN;
 
 const api = axios.create({
